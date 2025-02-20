1efc722d2807324bc6664d3e2ad60f566ec53d6a
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-02-20 09:42:51 +0530

    Milestone16 completed

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -5,7 +5,8 @@ import Home from "../pages/Home.jsx"
 import CreateProduct from "../pages/createProduct.jsx";
 import MyProducts from "../pages/myProduct.jsx";
 import Cart from "../pages/cart.jsx";
+import ProductDetails from "../pages/productDetails.jsx";
 
 
-export { Login,Signup,Home,CreateProduct,MyProducts,Cart}
+export { Login,Signup,Home,CreateProduct,MyProducts,Cart,ProductDetails}
 

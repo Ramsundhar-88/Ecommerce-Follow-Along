e76b4eb947c07c4c6e84133e7d5155abc2bdcdc6
Ramsundhar <69354316+Ramsundhar-88@users.noreply.github.com>
2025-03-07 11:40:06 +0530

    Update profile.jsx

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -16,7 +16,7 @@ export default function Profile() {
 
 	useEffect(() => {
     	fetch(
-			`http://localhost:8000/api/v2/user/profile?email=${"akshara@gmail.com"}`,
+			`http://localhost:8000/api/v2/user/profile?email=${"ramsundhar.sades@gmail.com"}`,
 			{
 				method: "GET",
 				headers: {
